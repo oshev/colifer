80328d46d20b65c8d548c01837ab0d0b91c79b4a
oshev <oshevelyov@gmail.com>
2015-05-19 19:12:24 +0200

    Added simple reading from a CSV file given in the configuration.

--- report-generator.py
+++ report-generator.py
@@ -1,1 +1,13 @@
-__author__ = 'oleg'
+import configparser
+import csv
+
+if __name__ == '__main__':
+    config = configparser.RawConfigParser()
+    config.read('report-generator.ini')
+    jiffyReport = config['Main' ]['jiffy_report']
+    print(jiffyReport)
+    with open(jiffyReport, newline='', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print(row)
+
