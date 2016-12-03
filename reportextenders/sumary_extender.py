7f457a9cca1736a6d74bbbedaca96b15a801092e
oleg-shevelev <oleg.shevelyov@schibsted.com>
2016-12-03 18:49:30 +0000

    Don't calculate time proportion if there is no time tracked.

--- reportextenders/sumary_extender.py
+++ reportextenders/sumary_extender.py
@@ -13,19 +13,20 @@ class SummaryExtender(ReportExtender):
     def extend_report(self, report, report_parameters):
         section_path_elements = self.summary_path.split(SECTION_SEPARATOR)
 
-        focused_time = int(round((report.root_section.stats.unit_stats.done_pomodoros * 25 * 60) /
-                                 report.root_section.stats.seconds * 100))
-        focused_units = int(round(report.root_section.stats.unit_stats.done_pomodoros /
-                                  report.root_section.stats.unit_stats.done_units() * 100))
-        focused_info = "{}% of highly focused time (ratio allTime/pomodoroTime) or {}% units.".\
-            format(focused_time, focused_units)
-        report.find_or_create_leaf(section_path_elements, focused_info)
+        if report.root_section.stats and report.root_section.stats.unit_stats:
+            focused_time = int(round((report.root_section.stats.unit_stats.done_pomodoros * 25 * 60) /
+                                     report.root_section.stats.seconds * 100))
+            focused_units = int(round(report.root_section.stats.unit_stats.done_pomodoros /
+                                      report.root_section.stats.unit_stats.done_units() * 100))
+            focused_info = "{}% of highly focused time (ratio allTime/pomodoroTime) or {}% units.".\
+                format(focused_time, focused_units)
+            report.find_or_create_leaf(section_path_elements, focused_info)
 
-        units_from_planned = int(round(report.root_section.stats.unit_stats.done_units_plan /
-                                       report.root_section.stats.unit_stats.planned * 100))
-        accomplished_info = "{}% units accomplished (ratio done from planned / planned.)". \
-            format(units_from_planned)
-        report.find_or_create_leaf(section_path_elements, accomplished_info)
+            units_from_planned = int(round(report.root_section.stats.unit_stats.done_units_plan /
+                                           report.root_section.stats.unit_stats.planned * 100))
+            accomplished_info = "{}% units accomplished (ratio done from planned / planned.)". \
+                format(units_from_planned)
+            report.find_or_create_leaf(section_path_elements, accomplished_info)
 
 
 
