e3b38c67e00cf529a8132d682dd4cb1141d9357a
Oleg Shevelev <oshevelyov@gmail.com>
2018-07-28 23:01:07 +0100

    Removed UnitStats (pomodoros).

--- colifer/reportextenders/toggl.py
+++ colifer/reportextenders/toggl.py
@@ -3,7 +3,6 @@ import requests
 from config import Config
 from reportextenders.report_extender import ReportExtender
 from sectionstats import SectionStats
-from sectionstats import UnitStats
 from datetime import datetime
 from datetime import timedelta
 from tag_order import TagOrder
@@ -54,12 +53,10 @@ class TogglEntriesParser(ReportExtender):
         entry_date = datetime.strptime(entry['start'][:CHARACTERS_IN_DATE], '%Y-%m-%d').date()
         days.add(entry_date)
         entry_duration = entry['duration']
-        unit_stats = UnitStats(done_pomodoros=(1 if MIN_POMODORO_IN_SECONDS < entry_duration < MAX_POMODORO_IN_SECONDS
-                                               else 0))
         tags = set(entry['tags']) if 'tags' in entry else set()
         sectionstat = SectionStats(path=SECTIONSTAT_PATH_SEPARATOR.join([project_name, entry['description']]),
                                    seconds=entry_duration, tags=tags,
-                                   days=days, unit_stats=unit_stats)
+                                   days=days)
         return sectionstat
 
     @staticmethod
--- colifer/sectionstats.py
+++ colifer/sectionstats.py
@@ -7,10 +7,9 @@ class SectionStats:
         self.days = self.days.union(stats.days)
         self.all_tags = self.all_tags.union(stats.all_tags)
         self.common_tags = self.common_tags.intersection(stats.common_tags)
-        self.unit_stats.add(stats.unit_stats)
 
     def __init__(self, path='', seconds=0, words_num=0, tags=set(),
-                 events_num=1, days=set(), comments_list=None, unit_stats=None):
+                 events_num=1, days=set(), comments_list=None):
         self.path = path
         self.seconds = seconds
         self.words_num = words_num
@@ -19,7 +18,6 @@ class SectionStats:
         self.events_num = events_num
         self.days = days
         self.comments_list = comments_list if comments_list else []
-        self.unit_stats = unit_stats if unit_stats else UnitStats()
 
     def get_sorted_and_formatted_days(self):
         return ", ".join([day.strftime("%m/%d %a") for day in sorted(list(self.days))])
@@ -34,8 +32,6 @@ class SectionStats:
             text += "{:d} times, ".format(self.events_num)
         if len(self.days) > 0:
             text += "{:d} days: {}; ".format(len(self.days), self.get_sorted_and_formatted_days())
-        if self.unit_stats is not None and not self.unit_stats.is_zero():
-            text += self.unit_stats.__str__()
         text = text.strip(', ').strip('; ')
         text += ')'
         return text
@@ -46,54 +42,5 @@ class SectionStats:
                ", \"Events num\": " + str(self.events_num) + \
                ", \"Days num\": " + str(len(self.days)) + \
                ", \"Days\": " + ("\"None\"" if self.days is None
-                                                else ", ".join(self.get_sorted_and_formatted_days())) + \
-               ", \"Comments list\": " + ("\"None\"" if self.comments_list is None else str(self.comments_list)) + \
-               ", \"Unit stats\": " + ("\"None\"" if self.unit_stats is None else str(self.unit_stats)) + "}"
-
-
-class UnitStats:
-
-    def __init__(self, planned=0, done_cucumbers=0, done_pomodoros=0, broken_pomodoros=0, not_done=0):
-        self.planned = planned
-        self.done_cucumbers = done_cucumbers  # cucumber is a non-focused interval (approx. 25 minutes)
-        self.done_pomodoros = done_pomodoros  # pomodoro is a focused and timed interval (usually 25 minutes)
-        self.done_units_plan = min(self.done_units(), planned)
-        self.broken_pomodoros = broken_pomodoros
-        self.not_done = not_done
-
-    def is_zero(self):
-        return not self.planned and not self.done_pomodoros and not self.done_cucumbers and \
-               not self.broken_pomodoros and not self.not_done
-
-    def add(self, unit_stats):
-        self.planned += unit_stats.planned
-        self.done_cucumbers += unit_stats.done_cucumbers
-        self.done_pomodoros += unit_stats.done_pomodoros
-        self.not_done += unit_stats.not_done
-        self.done_units_plan += unit_stats.done_units_plan
-        self.broken_pomodoros += unit_stats.broken_pomodoros
-
-    def done_units(self):
-        return self.done_cucumbers + self.done_pomodoros
-
-    def __repr__(self):
-        return "{\"Plan\": " + str(self.planned) + \
-               ", \"Units\": " + str(self.done_cucumbers) + \
-               ", \"Units Plan\": " + str(self.done_units_plan) + \
-               ", \"Poms\": " + str(self.done_pomodoros) + \
-               ", \"Broken\": " + str(self.broken_pomodoros) + \
-               ", \"Not done\": " + str(self.not_done) + "}"
-
-    @staticmethod
-    def fmt_field(name, value):
-        return "{}".format(name + ": " + str(int(value) if value - int(value) < 0.0001 else "{:.1f}".format(value)) +
-                           ", " if value > 0 else "")
-
-    def __str__(self):
-        return ("S[" + self.fmt_field("pln", self.planned) +
-                self.fmt_field("unt", self.done_units()) if self.done_units() != self.done_pomodoros else '' +
-                self.fmt_field("untPln", self.done_units_plan) +
-                self.fmt_field("pom", self.done_pomodoros) +
-                self.fmt_field("brk", self.broken_pomodoros) +
-                self.fmt_field("fail", self.not_done)).\
-                strip(', ') + "]" if not self.is_zero() else ""
+                                 else ", ".join(self.get_sorted_and_formatted_days())) + \
+               ", \"Comments list\": " + ("\"None\"" if self.comments_list is None else str(self.comments_list)) + "}"
