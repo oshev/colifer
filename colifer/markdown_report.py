9d71983d2a982efe2f82948d32f282b90c5df4ce
Oleg Shevelev <oshevelyov@gmail.com>
2021-01-08 21:37:47 +0000

    get rid of Mixed section in the report, style improvements, some type annotations

--- colifer/markdown_report.py
+++ colifer/markdown_report.py
@@ -1,44 +1,60 @@
+from reporting import Section, Report
+
 MAX_HEADER_LEVEL = 4
 
 
 class MarkdownReport:
 
     def traverse_and_save(self, md_file, section, level):
-        nice_stats = str(section.stats) if section.stats else ''
-        if section.stats and len(section.stats.all_tags) != len(section.stats.common_tags) \
-                and len(section.children) == 0:
-            all_tags = section.stats.all_tags
+        if section.name == 'Dummy':
+            return
+        stats_line = days_line = ''
+        is_header_level_section = section.holds_level_tag and level + 1 <= MAX_HEADER_LEVEL
+        # add statistics line (e.g. "(00:32, 2 times, 1 days) <!--11/05 Thu-->" only if useful
+        # skip stats for header sections with only 1 child (one child in header sections is always
+        # a Dummy section to make sure these sections are not deleted as empty)
+        if section.stats and (not is_header_level_section or len(section.children) > 2):
+            stats_line = f"*{str(section.stats)}*"
+            days_line = f"<!--{section.stats.get_sorted_and_formatted_days()}-->"
+        if is_header_level_section:
+            prefix_spaces = self.fill(level + 1)
+            section_name = f"\n{prefix_spaces} {section.name}\n"
         else:
-            all_tags = None
-        if section.name != 'Dummy':
-            if all_tags:
-                tags_line = f" *{', '.join(sorted(list(all_tags)))}*"
-            else:
-                tags_line = ''
+            prefix_spaces = self.fill(level + 1 - MAX_HEADER_LEVEL, '  ')
+            section_name = f"{prefix_spaces}- **{section.name}**"
+        all_tags = None
+        # print tags only for section where several items with the same name have different tag set
+        if (section.stats and len(section.stats.all_tags) != len(section.stats.common_tags)
+                and len(section.children) == 0):
+            all_tags = section.stats.all_tags
+        tags_line = f" *{', '.join(sorted(list(all_tags)))}*" if all_tags else ''
+        md_file.write(f"{section_name} {tags_line} {stats_line} {days_line}\n")
+        if section.children is not None and len(section.children) > 0:
+            for child in section.children:
+                self.traverse_and_save(md_file, child, level + 1)
 
-            if nice_stats:
-                stats_line = f"*{nice_stats}*"
-                days_line = f"<!--{section.stats.get_sorted_and_formatted_days()}-->"
-            else:
-                stats_line = days_line = ''
+            if section.stats is not None and section.stats.comments_list is not None:
+                for comment in section.stats.comments_list:
+                    md_file.write(f'{self.fill(level)}-{comment}\n')
 
-            if section.holds_level_tag and level + 1 <= MAX_HEADER_LEVEL:
-                prefix_spaces = self.fill(level + 1)
-                section_name = f"\n{prefix_spaces} {section.name}\n"
-            else:
-                prefix_spaces = self.fill(level + 1 - MAX_HEADER_LEVEL, '  ')
-                section_name = f"{prefix_spaces}- **{section.name}**"
+    @staticmethod
+    def prune_report(report: Report) -> None:
+        """
+        Remove higher levels up to the header sections for levels containing only 1 child.
 
-            md_file.write(f"{section_name} {tags_line} {stats_line} {days_line}\n")
+        E.g. "header1 -> section1 -> section2 -> section3" will become just "header1 -> section3",
+        and
+        "header2 -> section4 -> section5 -> section7
+                             -> section6"
+        will become  "header2 -> section4 -> section7
+                                          -> section6"
 
-            if section.children is not None and len(section.children) > 0:
-                for child in section.children:
-                    self.traverse_and_save(md_file, child, level + 1)
-
-                if section.stats is not None and section.stats.comments_list is not None:
-                    for comment in section.stats.comments_list:
-
-                        md_file.write(f'{self.fill(level)}-{comment}\n')
+        :param report: Report to prune.
+        :return: None, the action is done in-place (the original object is modified).
+        """
+        current_section = report.root_section
+        for child in current_section.children:
+            pass  # TODO
 
     def generate(self, report, filename, before_report, after_report):
         md_file = open(filename, "w")
@@ -54,6 +70,6 @@ class MarkdownReport:
     @staticmethod
     def fill(num, filler='#'):
         spaces = ''
-        for counter in range(0, num - 1):
+        for _ in range(0, num - 1):
             spaces += filler
         return spaces
